01d3660e538f0dd4dca47c88fc98390e54c69a6f
Cootz <artem2004799@gmail.com>
2024-11-24 11:32:29 +0300

    Layout basic interface

---
+++ AutoSales/View/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using AutoSales.View;
+using AutoSales.ViewModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoSales
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainViewModel viewModel => (MainViewModel)DataContext;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            viewModel.NavigationService = mainFrame.NavigationService;
+        }
+    }
+}
---
+++ AutoSales/ViewModel/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Navigation;
+using System.Windows.Controls;
+using AutoSales.View;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+
+namespace AutoSales.ViewModel
+{
+    public class MainViewModel : ObservableObject
+    {
+        private readonly Dictionary<string, Page> loadedPages = [];
+
+        public NavigationService NavigationService { get; set; } = null!;
+
+        public MainViewModel()
+        {
+            NavigateToSalesOverviewCommand = new RelayCommand(NavigateToPage<SalesOverveiw>);
+            NavigateToTestDataCommand = new RelayCommand(NavigateToPage<TestData>);
+        }
+
+        public ICommand NavigateToSalesOverviewCommand { get; }
+        public ICommand NavigateToTestDataCommand { get; }
+
+        private void NavigateToPage<T>() where T : Page, new()
+        {
+            string typeName = typeof(T).Name;
+
+            if (loadedPages.TryGetValue(typeName, out Page? page))
+            {
+                NavigationService.Navigate(page);
+            }
+            else
+            {
+                page = new T();
+
+                loadedPages.Add(typeName, page);
+            }
+        }
+    }
+}
